457612f1b17a97ede7516143cf06eaa0c8d8ba01
inamullah <inamullah.oraxtech@gmail.com>
2021-05-11 17:42:00 +0500

    functions with buttons

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,34 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+  void answerQuestion() => print(questionIndex);
+
   @override
   Widget build(BuildContext context) {
     var questions = [
       'What\'s your favorite colour?',
-      'What\'s your favorite animal?'
+      'What\'s your favorite animal?',
+      'What\'s your favorite celeberity?',
     ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Row(
+        body: Column(
           children: [
-            Text('The Questions'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
-            RaisedButton(child: Text('Answer 2'), onPressed: null),
-            RaisedButton(child: Text('Answer 3'), onPressed: null)
+            RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('Answer chosen 2!')),
+            RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () => print('Answer chosen 3!'))
           ],
         ),
       ),
